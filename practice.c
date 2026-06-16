891cfd60f507e733dbd774fcc1807f0b51a5b414
ankipop <ankipop8@gmail.com>
2026-06-16 17:25:47 +0530

    Update practice.c with input handling and print statements
    Refactored printing statements and added input handling for learning.

--- practice.c
+++ practice.c
@@ -6,13 +6,13 @@ int main()
 
 // PRINTING IN DIFFERENT LINES 
 
-   /* printf("Hello world! \n");
+   printf("Hello world! \n");
     printf("hello world! \n");
     
     printf("hello world! \n");
-   printf("hello world! \n");*/
+   printf("hello world! \n");
    
-   // PRINTING VARIABLES 
+// PRINTING VARIABLES 
    
    int age=22;
    float real_number=2.15;
@@ -24,7 +24,19 @@ int main()
    printf("decimal is %f \n" ,real_number);
    printf("string is %s \n" ,name);
    printf("for one letter or character is %c \n" ,name_oneletter);
-   
+ 
+//3.TO TAKE INPUT WE USE SCANF FUNCTION
+
+    int a,b;
+    printf("enter a: ");
+    scanf("%d", &a);
+    printf("enter b: ");
+    scanf("%d", &b);
+    printf("the sum is: %d", a+b);
+
+// OR ASSIGN THE VALUE TO A VARIABLE
+    int sum = a+b;
+    printf("the sum is: %d", sum);
    
     return 0;
 }
