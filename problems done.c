59fdc38423cbd5c9a768478bdad1867c83b0f16b
ankipop <ankipop8@gmail.com>
2026-06-17 13:17:36 +0530

    Update problems done.c

--- problems done.c
+++ problems done.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+9#include<stdio.h>
 #include<stdlib.h>
 
 int main()
@@ -18,7 +18,24 @@ printf("radius of the circle:  ");
 scanf("%f",&radius);
 float area=3.14*radius*radius;
 printf("the area is: %f ", area);
-    
+
+//P3. IMPLICIT CONVERSION   
+
+int num=(int) 1.9999;
+printf("%d", num);
+
+//P4.WRITE A PROGRAM TO CHECK IF A NUMBER IS DIVISIBLE BY 2
+
+int num;
+printf("type in your number: ");
+scanf("%d",&num);
+if(num%2==0){
+    printf("number is div by 2");
+}
+else{
+    printf("number is not div by 2");
+}
+
     
     return 0;
 }
