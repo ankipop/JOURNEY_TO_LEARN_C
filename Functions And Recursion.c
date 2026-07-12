7840413da373a71362011301b9cb0ebb3e884015
Daylight <ankipop8@gmail.com>
2026-07-12 21:00:23 +0530

    Add greeting functions and sum implementation

--- Functions And Recursion.c
+++ Functions And Recursion.c
@@ -10,3 +10,28 @@ int sum(int a,int b);
 
 int main()
 {
+
+
+
+  
+return 0;
+}
+
+void printhello(){
+   printf("hello world! \n");
+}
+
+void printgoodbye(){
+  printf("goodbye! \n");
+}
+
+void namaste(){
+  printf("namaste \n");
+}
+void bonjur(){
+  printf("bonjur \n");
+}
+
+int sum(int a,int b){
+  return a+b;
+}
